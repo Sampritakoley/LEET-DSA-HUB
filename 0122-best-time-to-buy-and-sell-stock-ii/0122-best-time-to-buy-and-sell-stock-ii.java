d285b39c4053f7bcc83706dbc5d7aea12d9162e7
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2025-05-22 21:27:42 +0530

    Time: 1 ms (78.75%), Space: 45.7 MB (71.55%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        boolean findMin=true;boolean findMax=false; boolean profit=false;
+        int min=prices[0]; int max=0; int p=0;
+        for(int i=1;i<prices.length;i++){
+            int val=prices[i];
+            
+             if(findMin){
+                if(val<min){
+                    min=val;
+                    continue;
+                }else{
+                    findMin=false;
+                    findMax=true;
+                }
+             }
+             if(findMax){
+                if(val>max){
+                    max=val;
+                    continue;
+                }else{
+                   p+=max-min;
+                   findMax=false;
+                   findMin=true;
+                   min=val;
+                   max=0;
+                }
+             }
+
+        }
+
+        if(findMax){
+            return p+(max-min);
+        }
+        return p;
+    }
+}

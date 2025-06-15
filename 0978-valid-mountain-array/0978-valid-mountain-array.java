f0b2777cddb29cf4c01855f6ab256ae1c16830a4
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2025-06-15 14:03:27 +0530

    Time: 2 ms (70.84%), Space: 45.4 MB (59.68%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.java
+++ 0978-valid-mountain-array/0978-valid-mountain-array.java
@@ -4,31 +4,19 @@ class Solution {
             return false;
         }
 
-        for(int i=1;i<arr.length-1;i++){
-              int left=i;
-              int right=i;
-              boolean res=true;
-              while(left>0){
-                if(arr[left]>arr[left-1]){
-                    left=left-1;
-                }else{
-                    res=false;
-                    break;
-                }
-              }
+        int i=0;
+        while(i+1<arr.length && arr[i]<arr[i+1]){
+            i++;
+        }
+        if(i==0 || i==arr.length-1){
+            return false;
+        }
 
-               while(right<arr.length-1){
-                if(arr[right]>arr[right+1]){
-                    right=right+1;
-                }else{
-                    res=false;
-                    break;
-                }
-              }
-
-              if(res){
-                return res;
-              }
+        while(i+1<arr.length && arr[i]>arr[i+1]){
+            i++;
+        }
+        if(i==arr.length-1){
+            return true;
         }
         return false;
     }

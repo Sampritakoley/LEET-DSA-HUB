b2ff355a0ad0a2eda5dd946bf03c6d4acbdd0d0e
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2025-07-02 23:04:29 +0530

    Time: 4 ms (86.03%), Space: 45.3 MB (65.74%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if (s.length() < k) return false;
+
+    int[] freq = new int[26];
+    for (char c : s.toCharArray()) {
+        freq[c - 'a']++;
+    }
+
+    int oddCount = 0;
+    for (int f : freq) {
+        if (f % 2 != 0) oddCount++;
+    }
+
+    return oddCount <= k;
+    }
+}

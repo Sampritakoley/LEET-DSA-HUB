4de63ffb6440cecc9679958df2e9f4d49534857e
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2025-07-20 11:26:03 +0530

    Time: 2 ms (55.48%), Space: 46.3 MB (26.02%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,78 +1,52 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int x=0;
-        int size=(nums1.length+nums2.length);
-        int mid=size%2==0?size/2:(size+1)/2;
-        int n1=0; int n2=0;
-        double res=0;int val=0;
-        while(n1<nums1.length &&  n2<nums2.length){
-
-            if(x==mid && size%2==1){
-              res=val;
-              return res;
-            }
-
-            if(x==mid && size%2==0){
-              res=val;
-              if(nums1[n1]<=nums2[n2]){
-                val=nums1[n1];
-                res+=val;
-              }else{
-                val=nums2[n2];
-                res+=val;
-              }
-              return res/2;
-            }
-
-            if(nums1[n1]<=nums2[n2]){
-                val=nums1[n1];
-                n1++;
-                x++;
-            }
-
-            else{
-                 val=nums2[n2];
-                n2++;
-                x++;
-            }
-        }
-         
-        while(n1<nums1.length && x<mid){
-             val=nums1[n1];
-                n1++;
-                x++;
-        }
-
-        while(n2<nums2.length && x<mid){
-             val=nums2[n2];
-                n2++;
-                x++;
-        }
-
-
-        res=val;
-        if(size%2==0){
-            if(n1<nums1.length && n2<nums2.length){
-              if(nums1[n1]<=nums2[n2]){
-                val=nums1[n1];
-                res+=val;
-              }else{
-                val=nums2[n2];
-                res+=val;
-              }
-            }
-
-            else if(n1<nums1.length){
-                 val=nums1[n1];
-                res+=val;
+        int n=nums1.length+nums2.length;
+        int ind2=n/2;
+        int ind1=ind2-1;
+        int e1=0;int e2=0;
+        int i=0;int j=0;
+        int cnt=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<=nums2[j]){
+                if(cnt==ind1){
+                    e1=nums1[i];
+                } if(cnt==ind2){
+                    e2=nums1[i];
+                }
+                cnt++;
+                i++;
             }else{
-                 val=nums2[n2];
-                res+=val;
+                if(cnt==ind1){
+                    e1=nums2[j];
+                } if(cnt==ind2){
+                    e2=nums2[j];
+                }
+                cnt++;
+                j++;
             }
-            return res/2;
+        }
+        while(i<nums1.length){
+             if(cnt==ind1){
+                    e1=nums1[i];
+                } if(cnt==ind2){
+                    e2=nums1[i];
+                }
+                cnt++;
+                i++;
+        }
+        while(j<nums2.length){
+            if(cnt==ind1){
+                    e1=nums2[j];
+                } if(cnt==ind2){
+                    e2=nums2[j];
+                }
+                cnt++;
+                j++;
         }
 
-        return res;
-        
+        if(n%2!=0){
+            return e2;
+        }
+        return ((double)(e1+e2))/2.0;
     }
 }

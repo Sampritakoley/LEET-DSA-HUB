c13b49dd29463603dd817d636b5a3c5386addf6f
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2025-11-03 00:56:53 +0530

    Time: 0 ms (100%), Space: 46.8 MB (5.43%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,17 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if (image[sr][sc]==color) return image;
-        dfs(image,sr,sc,color,image[sr][sc]);
+        if(image[sr][sc]==color){
+            return image;
+        }
+        boolean[][] visited=new boolean[image.length][image[0].length];
+        int oldC=image[sr][sc];
+        dfs(image,visited,oldC,color,sr,sc);
         return image;
-    }public static void dfs(int[][]image,int i,int j,int c,int oldC){
-         if(i<0||i>=image.length|| j<0||j>=image[0].length||image[i][j]==c||
-         image[i][j]!=oldC){
-              return;
-         }
-         image[i][j]=c;
-         dfs(image,i+1,j,c,oldC);
-         dfs(image,i-1,j,c,oldC);
-         dfs(image,i,j+1,c,oldC);
-         dfs(image,i,j-1,c,oldC);
+    }
+    public void dfs(int[][] image,boolean[][] visited,int oldC,int newC,int i,int j){
+        if(i<0 || j< 0 || i>= image.length || j>= image[0].length || image[i][j]!=oldC || visited[i][j]){
+            return;
+        }
+        visited[i][j]=true;
+        image[i][j]=newC;
+        dfs(image,visited,oldC,newC,i-1,j);
+        dfs(image,visited,oldC,newC,i+1,j);
+        dfs(image,visited,oldC,newC,i,j-1);
+        dfs(image,visited,oldC,newC,i,j+1);
     }
 }

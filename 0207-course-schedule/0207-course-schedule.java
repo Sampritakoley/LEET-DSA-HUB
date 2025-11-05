5cccfef6a3103b79c6a5a924e3fdac86ff64e4f2
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2025-11-05 13:09:49 +0530

    Time: 6 ms (73.08%), Space: 47 MB (5.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,34 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph=new ArrayList<>();
+        ArrayList<Integer>[] graph=new ArrayList[numCourses];
+        int cmpt=0;
         int[] indegree=new int[numCourses];
         for(int i=0;i<numCourses;i++){
-            graph.add(new ArrayList<>());
+            graph[i]=new ArrayList<>();
         }
-
         for(int i=0;i<prerequisites.length;i++){
-            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
+            int cource=prerequisites[i][0];
+            int preReq=prerequisites[i][1];
+            graph[preReq].add(cource);
+            indegree[cource]++;
         }
-
-        Queue<Integer> q=new LinkedList<>();
+        Queue<Integer> que=new LinkedList<>();
         for(int i=0;i<indegree.length;i++){
             if(indegree[i]==0){
-                q.add(i);
+                que.add(i);
             }
         }
+        while(que.size()>0){
+            cmpt++;
+            int node=que.poll();
+            for (int next : graph[node]) {
 
-        int count=0;
-        while(q.size()!=0){
-            int n=q.poll();
-            count++;
-            for(int nb:graph.get(n)){
-                indegree[nb]--;
-                if(indegree[nb]==0){
-                    q.add(nb);
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    que.add(next);
                 }
             }
         }
-        return count==numCourses;
+        return cmpt==numCourses?true:false;
     }
 }

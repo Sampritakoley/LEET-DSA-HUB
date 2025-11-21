0c0971f0ea99216254d83a7bf221774e0e248a43
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2025-11-22 00:09:54 +0530

    Time: 1 ms (100%), Space: 43.9 MB (11%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,26 +1,35 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0, n = s.length();
-        while (i < n && s.charAt(i) == ' ') {
-            i++;
-        }
+        int index = 0;
+        int n = s.length();
+
+        while (index < n && s.charAt(index) == ' ') index++;
+
+        if (index >= n) return 0;
 
         int sign = 1;
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
-            i++;
+        int res = 0;
+
+        if (s.charAt(index) == '-' || s.charAt(index) == '+') {
+            if (s.charAt(index) == '-') sign = -1;
+            index++;
         }
 
-        long result = 0;
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            result = result * 10 + (s.charAt(i) - '0');
-
-            if (result * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (result * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-
-            i++;
+        return convert(s, res, index, sign);
+    }
+     public static int convert(String s, int res, int index, int sign) {
+        if (index >= s.length() || !Character.isDigit(s.charAt(index))) {
+            return sign == 1 ? res : -res;
         }
 
-        return (int) (result * sign);
+        int currDigit = s.charAt(index) - '0';
+
+        if (res > (Integer.MAX_VALUE - currDigit) / 10) {
+            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+
+        res = res * 10 + currDigit;
+
+        return convert(s, res, index + 1, sign);
     }
 }

54c6a9d62e3177331c21b5c24ee26e2da48ad7b9
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2025-11-26 12:03:55 +0530

    Time: 7 ms (16.32%), Space: 45.1 MB (29.32%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -2,20 +2,13 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res=new ArrayList<List<Integer>>();
         List<Integer> ans=new ArrayList<Integer>();
-        Set<String> uniqueSet = new HashSet<>();
         Arrays.sort(candidates);
-        findCombination(res,ans,candidates,target,uniqueSet,0);
+        findCombination(res,ans,candidates,target,0);
         return res; 
     }
-    public static void findCombination(List<List<Integer>> res, List<Integer> ans,int[] candidates, int target,Set<String> uniqueSet,int index){
+    public static void findCombination(List<List<Integer>> res, List<Integer> ans,int[] candidates, int target,int index){
         if(target==0){
-            List<Integer> temp = new ArrayList<>(ans);
-            Collections.sort(temp);
-            String key = temp.toString();
-            if (!uniqueSet.contains(key)) {
-                uniqueSet.add(key);
-                res.add(temp);
-            }
+            res.add(new ArrayList<>(ans));
             return;
         }
         if(target<0){
@@ -23,11 +16,11 @@ class Solution {
         }
 
         for(int i=index;i<candidates.length;i++){
-            if (i > index && candidates[i] == candidates[i - 1]) continue;
+            if (i>index && candidates[i] == candidates[i - 1]) continue;
             int val=candidates[i];
             int restTarget=target-val;
             ans.add(val);
-            findCombination(res, ans, candidates, restTarget,uniqueSet, i+1);
+            findCombination(res, ans, candidates, restTarget, i+1);
             ans.remove(ans.size() - 1);
         }
     }

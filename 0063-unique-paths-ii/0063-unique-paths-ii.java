dbd531e0a5e5d2434cc5a800da24923c1bba8338
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-01-14 11:04:49 +0530

    Time: 0 ms (100%), Space: 43.5 MB (67.87%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,34 +1,42 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int rowCount=obstacleGrid.length;
-        int colCount=obstacleGrid[0].length;
-        int[][] dp=new int[rowCount][colCount];
-        dp[rowCount-1][colCount-1]=obstacleGrid[rowCount-1][colCount-1]==1?0:1;
-        for(int i=dp[0].length-2;i>=0;i--){
-            if(obstacleGrid[obstacleGrid.length-1][i]==1){
-                dp[obstacleGrid.length-1][i]=0;
-            }else{
-                dp[obstacleGrid.length-1][i]= dp[obstacleGrid.length-1][i+1];
-            }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
         }
-        for(int i=dp.length-2;i>=0;i--){
-            if(obstacleGrid[i][obstacleGrid[0].length-1]==1){
-                dp[i][obstacleGrid[0].length-1]=0;
-            }else{
-                dp[i][obstacleGrid[0].length-1]= dp[i+1][obstacleGrid[0].length-1];
+
+        int[][] dp = new int[m][n];
+
+        dp[0][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = dp[i - 1][0];
             }
         }
 
-        for(int i=dp.length-2;i>=0;i--){
-            for(int j=dp[0].length-2;j>=0;j--){
-                if(obstacleGrid[i][j]==1){
-                    dp[i][j]=0;
-                }else{
-                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                dp[0][j] = 0;
+            } else {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
             }
         }
 
-        return dp[0][0];
+        return dp[m - 1][n - 1];
     }
 }

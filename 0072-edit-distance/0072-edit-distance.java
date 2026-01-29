1dcb10626d9778719a22f56baa262dec54bb0718
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-01-30 01:21:10 +0530

    Time: 5 ms (65.66%), Space: 45 MB (96.77%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -10,7 +10,7 @@ class Solution {
         for(int i=1;i<dp.length;i++){
             for(int j=1;j<dp[0].length;j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j]=Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
+                    dp[i][j]= dp[i-1][j-1];
                 }else{
                     dp[i][j]=Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1])+1;
                 }

3e6147bcf7e17a20433afe3456d4c3deeea833b9
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-02-22 00:24:24 +0530

    Time: 2 ms (97.66%), Space: 41.9 MB (99.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -6,7 +6,7 @@ class Solution {
             if(ch=='(' || ch=='['|| ch=='{'){
                st.push(ch);
             }else{
-                if(!st.isEmpty() && (ch==')' && st.peek()=='(') || (ch=='}' && st.peek()=='{')||(ch==']' && st.peek()=='[')){
+                if(!st.isEmpty() && ((ch==')' && st.peek()=='(') || (ch=='}' && st.peek()=='{')||(ch==']' && st.peek()=='['))){
                     st.pop();
                 }else{
                    return false;

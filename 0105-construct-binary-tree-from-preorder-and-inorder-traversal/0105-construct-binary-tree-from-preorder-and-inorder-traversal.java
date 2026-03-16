759b1836a34ab31b3c7d26927c9517887146282d
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-03-17 02:17:23 +0530

    Time: 3 ms (30.59%), Space: 45.6 MB (82.35%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,29 +15,37 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        mapInorderIndex(inorder,map);
-        TreeNode root=ConstructTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1,map);
-        return root;
-    }public void mapInorderIndex(int[] inorder,HashMap<Integer,Integer> map){
-         int i=0;
-         while(i<inorder.length){
-            map.put(inorder[i],i);
-            i++;
-         }
-    }public TreeNode ConstructTree(int[] preorder,int preSt,int preEd,int[] inorder,int inSt,int inEd,HashMap<Integer,Integer> map) {
+        int n=preorder.length-1;
+        TreeNode node=Constract(preorder,0,n,inorder,0,n);
+        return node;
+    }public TreeNode Constract(int[]preorder, int preSt, int preEd, int[] inorder, int inSt,int inEd){
         if(preSt>preEd || inSt>inEd){
             return null;
         }
-         
-        int rootVal = preorder[preSt];    
-        int rootIndex = map.get(rootVal);
+        
+        int rootVal=preorder[preSt];
         TreeNode root=new TreeNode(rootVal);
-        int noLeft=rootIndex-inSt;
-        TreeNode leftNode=ConstructTree(preorder,preSt+1,preSt+noLeft,inorder,inSt,rootIndex-1,map);
-        TreeNode rightNode=ConstructTree(preorder,preSt+noLeft+1,preEd,inorder,rootIndex+1,inEd,map);
-        root.left=leftNode;
-        root.right=rightNode;
+        int rootIndex=indexOf(inorder,rootVal);
+        int noOfLeft=rootIndex-inSt;
+
+        TreeNode leftSubtree=Constract(preorder,preSt+1,preSt+noOfLeft,inorder,inSt,rootIndex-1);
+        TreeNode rightSubtree=Constract(preorder,preSt+noOfLeft+1,preEd,inorder,rootIndex+1,inEd);
+
+        root.left=leftSubtree;
+        root.right=rightSubtree;
         return root;
     }
+
+     public static int indexOf(int[] arr, int target) {
+        if (arr == null) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; 
+            }
+        }
+        return -1; 
+    }
 }
+

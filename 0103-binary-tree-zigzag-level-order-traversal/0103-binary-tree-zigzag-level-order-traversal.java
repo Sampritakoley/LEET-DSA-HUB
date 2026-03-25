1b946ec2d246f3e801be7df87664b3c4090f1ae4
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-03-25 12:15:46 +0530

    Time: 1 ms (77.48%), Space: 43.7 MB (73.41%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,32 +15,33 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        boolean l2r = true;
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            List<Integer> level = new ArrayList<>(Collections.nCopies(levelSize, 0));
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.poll();
-
-                int index = l2r ? i : levelSize - 1 - i;
-                level.set(index, node.val);
-
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
-
-            result.add(level);
-            l2r = !l2r;
+        ArrayList<List<Integer>> res=new ArrayList<>();
+        if(root==null){
+            return res;
         }
+        Queue<TreeNode> q=new LinkedList<>(); boolean isFromLeft=false;
+        q.offer(root);
+        while(q.size()>0){
+            int levelSize=q.size();
+            ArrayList<Integer> list=new ArrayList<>();
+            for(int i=0;i<levelSize;i++){
+                TreeNode current=q.poll();
+                if(isFromLeft){
+                    list.addFirst(current.val);
+                }else{
+                    list.addLast(current.val);
+                }
 
-        return result;
+                if(current.left!=null){
+                    q.offer(current.left);
+                }
+                if(current.right!=null){
+                    q.offer(current.right);
+                }
+            }
+            isFromLeft=!isFromLeft;
+            res.add(list);
+        }
+        return res;
     }
 }

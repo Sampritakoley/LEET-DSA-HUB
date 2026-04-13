7ef4af0125b928fb939844d38599faa3b5b98924
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-04-13 18:54:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        if(n==2){
+            return Math.max(nums[0],nums[1]);
+        }
+        int result1=findMaxRob(nums,0,n-1);
+        int result2=findMaxRob(nums,1,n);
+        return Math.max(result1,result2);
+    }
+    public static int findMaxRob(int[]nums, int st,int ed){
+        int pre=nums[st];
+        int preNext=nums[st+1];
+        for(int i=st+2;i<ed;i++){
+            int val=Math.max(preNext,pre+nums[i]);
+            pre=preNext;
+            preNext=val;
+        }
+        return preNext;
+    }
+}

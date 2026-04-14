3cf9bc528b50ac11b1f089850cb884770877053f
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-04-15 00:50:16 +0530

    Time: 40 ms (95.16%), Space: 44.2 MB (84.96%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -7,23 +7,16 @@ class Solution {
         for (int i = 0; i < n; i++) {
             arr[i + 1] = nums[i];
         }
-
         int[][] dp = new int[n + 2][n + 2];
-        for (int len = 1; len <= n; len++) {
-            for (int i = 1; i + len - 1 <= n; i++) {
-                int j = i + len - 1;
-
-                for (int k = i; k <= j; k++) {
-                    int coins =
-                        arr[i - 1] * arr[k] * arr[j + 1]
-                        + dp[i][k - 1]
-                        + dp[k + 1][j];
-
+        for (int len = 2; len < n + 2; len++) { 
+            for (int i = 0; i + len < n + 2; i++) {
+                int j = i + len;
+                for (int k = i + 1; k < j; k++) {
+                    int coins = dp[i][k] + dp[k][j] + arr[i] * arr[k] * arr[j];
                     dp[i][j] = Math.max(dp[i][j], coins);
                 }
             }
         }
-
-        return dp[1][n];
+        return dp[0][n + 1];
     }
 }

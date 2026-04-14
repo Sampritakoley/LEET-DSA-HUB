5c762215a8e96f6b6d9a484e2968a20eacd9c18a
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-04-14 13:19:32 +0530

    Time: 3 ms (53.95%), Space: 48.5 MB (39.24%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,42 +1,28 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        int n = s.length();
-        Set<String> dict = new HashSet<>(wordDict);
-
-        List<Integer>[] dp = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) dp[i] = new ArrayList<>();
-
-        dp[0].add(0); 
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (!dp[j].isEmpty() && dict.contains(s.substring(j, i))) {
-                    dp[i].add(j); 
+        Set<String> set=new HashSet<>(wordDict);
+        ArrayList<String>[] dp=new ArrayList[s.length()+1];
+        dp[0]=new ArrayList<>();
+        dp[0].add("");
+        for(int i=1;i<s.length()+1;i++){
+            dp[i]=new ArrayList<>();
+            for(int j=i-1;j>=0;j--){
+                String str=s.substring(j,i);
+                if(set.contains(str)){
+                    ArrayList<String> lst=dp[j];
+                    if(lst.size()==0){
+                        continue;
+                    }
+                    for(String prev:lst){
+                        if(prev.isEmpty()){
+                           dp[i].add(str);   
+                        }else{
+                           dp[i].add(prev+" "+str);
+                        }
+                    }
                 }
             }
         }
-
-        List<String> result = new ArrayList<>();
-        if (dp[n].isEmpty()) return result; 
-
-        backtrack(s, dp, n, new ArrayList<>(), result);
-        return result;
-    }
-   private void backtrack(String s, List<Integer>[] dp, int index,
-                           List<String> path, List<String> result) {
-
-        if (index == 0) {
-            Collections.reverse(path);
-            result.add(String.join(" ", path));
-            Collections.reverse(path);
-            return;
-        }
-
-        for (int start : dp[index]) {
-            String word = s.substring(start, index);
-            path.add(word);
-            backtrack(s, dp, start, path, result);
-            path.remove(path.size() - 1);
-        }
+        return dp[s.length()];
     }
 }

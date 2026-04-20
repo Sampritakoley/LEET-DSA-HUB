9a5e52a8d53610acb26d756d44d7ada2a5dfba54
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-04-21 01:02:43 +0530

    Time: 1 ms (100%), Space: 45.6 MB (26.68%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -15,24 +15,30 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> res = new ArrayList<>();
-        findSum(root, targetSum, res,new ArrayList<>());
-        return res;
-    }
-    public void findSum(TreeNode root, int targetSum,List<List<Integer>> res,List<Integer> list){
-        
+        ArrayList<List<Integer>> res=new ArrayList<>();
+        ArrayList<Integer> list=new ArrayList<>();
         if(root==null){
+            return res;
+        }
+        addPathSum(res,list,root,targetSum);
+        return res;
+    }public void addPathSum(List<List<Integer>> res,List<Integer> list,TreeNode node,int target){
+        if(node.left==null &&  node.right==null && node.val==target){
+            list.add(node.val);
+            res.add(new ArrayList<>(list));
+            list.remove(list.size()-1);
             return;
         }
-        list.add(root.val);
-        if(targetSum==root.val && root.left==null && root.right==null){
-            res.add(new ArrayList<>(list));
+        if(node.left==null &&  node.right==null && node.val!=target){
+            return;
         }
-        
-        
-        findSum(root.left,targetSum-root.val,res,list);
-        findSum(root.right,targetSum-root.val,res,list);
+        list.add(node.val);
+        if(node.left!=null){
+            addPathSum(res,list,node.left,target-node.val);
+        }
+        if(node.right!=null){
+            addPathSum(res,list,node.right,target-node.val);
+        }
         list.remove(list.size()-1);
-        return;
     }
 }

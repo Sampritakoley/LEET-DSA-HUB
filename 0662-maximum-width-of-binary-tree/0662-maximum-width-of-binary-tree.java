37a45e11c7486d27f19da91955f615939b694060
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-06-11 15:10:17 +0530

    Time: 1 ms (99.43%), Space: 45.2 MB (48%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,39 +14,59 @@
  * }
  */
 class Solution {
-    public class Pair{
-        public TreeNode node;
-        public long index;
-        public Pair(TreeNode node,long index){
-            this.node=node;
-            this.index=index;
+   class Pair {
+        TreeNode node;
+        long index;
+
+        Pair(TreeNode node, long index) {
+            this.node = node;
+            this.index = index;
         }
-    }
-    //index 2x,2x+1 (0- based indexing)
-    public int widthOfBinaryTree(TreeNode root) {
-        Queue<Pair> q=new LinkedList<>();
-        q.offer(new Pair(root,0));int max=0;
-        while(q.size()>0){
-           int levelSize=q.size(); long first=0;long last=0;
-           for(int i=0;i<levelSize;i++){
-                Pair curr=q.poll();
-                TreeNode currNode=curr.node;
-                if(i==0){
-                    first=curr.index;
-                }
-                if(i==levelSize-1){
-                    last=curr.index;
-                }
-                if(currNode.left!=null){
-                    q.offer(new Pair(currNode.left,curr.index*2));
-                }
-                if(currNode.right!=null){
-                    q.offer(new Pair(currNode.right,(curr.index*2)+1));
-                }
-           }
-           int width=(int) ((last-first)+1);
-           max=width>max?width:max;
+    }public int widthOfBinaryTree(TreeNode root) {
+
+        if (root == null) {
+            return 0;
         }
-        return max;
+
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, 0));
+
+        int maxWidth = 0;
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            long first = q.peek().index;
+            long last = first;
+
+            for (int i = 0; i < size; i++) {
+
+                Pair curr = q.poll();
+
+                long idx = curr.index;
+
+                last = idx;
+
+                if (curr.node.left != null) {
+                    q.offer(
+                        new Pair(curr.node.left, 2 * idx)
+                    );
+                }
+
+                if (curr.node.right != null) {
+                    q.offer(
+                        new Pair(curr.node.right, 2 * idx + 1)
+                    );
+                }
+            }
+
+            maxWidth = Math.max(
+                maxWidth,
+                (int)(last - first + 1)
+            );
+        }
+
+        return maxWidth;
     }
 }

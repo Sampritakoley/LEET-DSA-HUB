fe768bb6ef87fba70a0fd723363ef9afddb22eb9
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-06-16 23:42:32 +0530

    Time: 7 ms (97.37%), Space: 48.2 MB (33.65%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -1,38 +1,52 @@
+class Codec {
 
-public class Codec {
     public String serialize(TreeNode root) {
+
         StringBuilder sb = new StringBuilder();
-        helper(root, sb);
+
+        preorder(root, sb);
+
         return sb.toString();
     }
 
-    private void helper(TreeNode node, StringBuilder sb) {
-        if (node == null) {
-            sb.append("null,");
+    private void preorder(TreeNode root,
+                          StringBuilder sb) {
+
+        if (root == null) {
+            sb.append("N,");
             return;
         }
 
-        sb.append(node.val).append(",");
-        helper(node.left, sb);
-        helper(node.right, sb);
+        sb.append(root.val).append(",");
+
+        preorder(root.left, sb);
+        preorder(root.right, sb);
     }
+
+    int index;
 
     public TreeNode deserialize(String data) {
+
+        index = 0;
+
         String[] arr = data.split(",");
-        int[] index = new int[1]; // pointer
-        return build(arr, index);
+
+        return build(arr);
     }
 
-    private TreeNode build(String[] arr, int[] index) {
-        if (arr[index[0]].equals("null")) {
-            index[0]++;
+    private TreeNode build(String[] arr) {
+
+        if (arr[index].equals("N")) {
+            index++;
             return null;
         }
 
-        TreeNode node = new TreeNode(Integer.parseInt(arr[index[0]++]));
-        node.left = build(arr, index);
-        node.right = build(arr, index);
+        TreeNode root =
+            new TreeNode(Integer.parseInt(arr[index++]));
 
-        return node;
+        root.left = build(arr);
+        root.right = build(arr);
+
+        return root;
     }
 }

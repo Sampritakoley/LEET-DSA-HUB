25cfa03ae783cebca80db7d84666582b2b0df859
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-06-26 04:23:37 +0530

    Time: 6 ms (85.62%), Space: 46.9 MB (10.79%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,50 +1,23 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] cost=new int[n];
+        Arrays.fill(cost,Integer.MAX_VALUE);
+        int count=0; cost[src]=0;
+        while(k>=count){
+            int[] temp=cost.clone();
+            for(int i=0;i<flights.length;i++){
+                int[] f=flights[i];
+                int u=f[0];
+                int v=f[1];
+                int p=f[2];
 
-        List<int[]>[] graph = new ArrayList[n];
-
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for (int[] flight : flights) {
-            graph[flight[0]].add(new int[]{flight[1], flight[2]});
-        }
-
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{src, 0});
-
-        int[] minCost = new int[n];
-        Arrays.fill(minCost, Integer.MAX_VALUE);
-        minCost[src] = 0;
-
-        int stops = 0;
-
-        while (!queue.isEmpty() && stops <= k) {
-
-            int size = queue.size();
-
-            while (size-- > 0) {
-
-                int[] curr = queue.poll();
-                int city = curr[0];
-                int cost = curr[1];
-
-                for (int[] neighbor : graph[city]) {
-
-                    int nextCity = neighbor[0];
-                    int nextCost = cost + neighbor[1];
-
-                    if (nextCost < minCost[nextCity]) {
-                        minCost[nextCity] = nextCost;
-                        queue.offer(new int[]{nextCity, nextCost});
-                    }
+                if(cost[u]!=Integer.MAX_VALUE){
+                    temp[v]=Math.min(temp[v],cost[u]+p);
                 }
             }
-
-            stops++;
+            cost=temp;
+            count++;
         }
-
-        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
+        return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
     }
 }

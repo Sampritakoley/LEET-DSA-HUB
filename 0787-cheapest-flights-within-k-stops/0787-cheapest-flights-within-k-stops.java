61024db989b5fbd0f57e5929f4569de9e9d5145c
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-06-26 04:03:24 +0530

    Time: 6 ms (85.62%), Space: 46.3 MB (83.05%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,51 +1,50 @@
 class Solution {
-    public static class Pair{
-        int n;
-        int cost;
-        int stopage;
-        public Pair(int n,int cost,int stopage){
-            this.n=n;
-            this.cost=cost;
-            this.stopage=stopage;
-        }
-    }
-    public int findCheapestPrice(int n, int[][] flights,
-     int src, int dst, int k) {
-        Queue<Pair> q=new LinkedList<>();
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
         List<int[]>[] graph = new ArrayList[n];
+
         for (int i = 0; i < n; i++) {
             graph[i] = new ArrayList<>();
         }
+
         for (int[] flight : flights) {
-            int u = flight[0];
-            int v = flight[1];
-            int cst = flight[2];
-            graph[u].add(new int[]{v, cst});
+            graph[flight[0]].add(new int[]{flight[1], flight[2]});
         }
 
-        int[] mincost=new int[n];
-        Arrays.fill(mincost,Integer.MAX_VALUE);
-        mincost[src]=0;
-        int ans=Integer.MAX_VALUE;
-        q.add(new Pair(src,0,0));
-        while(q.size()>0){
-            Pair p=q.poll();
-            if(dst==p.n){
-                ans=Math.min(ans,p.cost);
-            }
-            if (p.stopage > k) continue;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{src, 0}); // city, cost
 
-            for (int[] edge : graph[p.n]) {
-                int nb_n = edge[0];
-                int nb_ct = edge[1];
-                int newcost = p.cost + nb_ct;
+        int[] minCost = new int[n];
+        Arrays.fill(minCost, Integer.MAX_VALUE);
+        minCost[src] = 0;
 
-                if (newcost < mincost[nb_n]) {
-                    mincost[nb_n] = newcost;
-                    q.offer(new Pair(nb_n, newcost, p.stopage + 1));
+        int stops = 0;
+
+        while (!queue.isEmpty() && stops <= k) {
+
+            int size = queue.size();
+
+            while (size-- > 0) {
+
+                int[] curr = queue.poll();
+                int city = curr[0];
+                int cost = curr[1];
+
+                for (int[] neighbor : graph[city]) {
+
+                    int nextCity = neighbor[0];
+                    int nextCost = cost + neighbor[1];
+
+                    if (nextCost < minCost[nextCity]) {
+                        minCost[nextCity] = nextCost;
+                        queue.offer(new int[]{nextCity, nextCost});
+                    }
                 }
             }
+
+            stops++;
         }
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+
+        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
     }
 }

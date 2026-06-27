ae4ba236e174ab130f04d1d442275dc6147449c2
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-06-27 22:20:01 +0530

    Time: 99 ms (46.42%), Space: 128 MB (10.72%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.java
+++ 0815-bus-routes/0815-bus-routes.java
@@ -1,68 +1,49 @@
-import java.util.*;
-
 class Solution {
-
     public int numBusesToDestination(int[][] routes, int source, int target) {
-
-        if (source == target) {
+        HashMap<Integer,HashSet<Integer>> map=new HashMap<>();
+        buildMap(routes,map);
+        Queue<Integer> q=new ArrayDeque<>();
+        HashSet<Integer> visitedBus=new HashSet<>();
+        HashSet<Integer> visitedStops = new HashSet<>();
+        if(source==target){
             return 0;
         }
-
-        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
-        buildMap(routes, map);
-
-        Queue<Integer> q = new LinkedList<>();
-        HashSet<Integer> visitedBus = new HashSet<>();
-        HashSet<Integer> visitedStops = new HashSet<>();
-
-        q.offer(source);
-        visitedStops.add(source);
-
-        int buses = 0;
-
-        while (!q.isEmpty()) {
-
-            int size = q.size();
-            buses++; 
-
-            while (size-- > 0) {
-
-                int stop = q.poll();
-
-                if (!map.containsKey(stop))
-                    continue;
-
-                for (int bus : map.get(stop)) {
-
-                    if (visitedBus.contains(bus))
-                        continue;
-
+        q.add(source); visitedStops.add(source); int buses=1;
+        while(q.size()>0){
+            int size=q.size();
+            while(size-- >0){
+                Integer c=q.poll();
+                if (!map.containsKey(c))
+                continue;
+                for(int bus:map.get(c)){
+                    if(visitedBus.contains(bus)){
+                       continue;
+                    }
                     visitedBus.add(bus);
-
-                    for (int nextStop : routes[bus]) {
-
-                        if (nextStop == target)
-                            return buses;
-
-                        if (visitedStops.add(nextStop)) {
-                            q.offer(nextStop);
+                    for(int stops:routes[bus]){
+                        
+                        if(stops==target){
+                           return buses;
+                        }
+                        if(visitedStops.add(stops)){
+                            q.offer(stops);
                         }
                     }
                 }
             }
+            buses++;
         }
-
         return -1;
-    }
-
-    public void buildMap(int[][] routes, HashMap<Integer, HashSet<Integer>> map) {
-
-        for (int bus = 0; bus < routes.length; bus++) {
-
-            for (int stop : routes[bus]) {
-
-                map.computeIfAbsent(stop, k -> new HashSet<>()).add(bus);
+    }public void buildMap(int[][] routes,HashMap<Integer,HashSet<Integer>> map){
+        int i=0;
+        for(int[] r:routes){
+            for(int num:r){
+                if(!map.containsKey(num)){
+                    map.put(num,new HashSet<>());
+                }
+                map.get(num).add(i);
             }
+            i++;
         }
     }
 }

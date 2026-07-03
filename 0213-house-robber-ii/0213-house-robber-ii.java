efb66fa3cc27ba917fbf1cdeefeafceba73a6eb0
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-04 01:42:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (40.72%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1){
-            return nums[0];
+        int n=nums.length;int robHouse2=0;
+        int robHouse1=findMaxRobbedAmt(nums,0,n-1);
+        if(n>=2){
+            robHouse2=findMaxRobbedAmt(nums,1,n);
         }
-        if(n==2){
-            return Math.max(nums[0],nums[1]);
+        return Math.max(robHouse1,robHouse2);
+    }public int findMaxRobbedAmt(int[] nums,int st,int ed){
+        int len=nums.length-1;
+        int prev2=nums[st];
+        int prev1=nums[st];
+        if(len>1){
+           prev1=Math.max(nums[st],nums[st+1]);
+        }else{
+            return prev2;
         }
-        int result1=findMaxRob(nums,0,n-1);
-        int result2=findMaxRob(nums,1,n);
-        return Math.max(result1,result2);
-    }
-    public static int findMaxRob(int[]nums, int st,int ed) {
-        int pre=nums[st];
-        int preNext=Math.max(nums[st+1],pre);
+        
         for(int i=st+2;i<ed;i++){
-            int val=Math.max(preNext,pre+nums[i]);
-            pre=preNext;
-            preNext=val;
+            int current=Math.max(prev2+nums[i],prev1);
+            prev2=prev1;
+            prev1=current;
         }
-        return preNext;
+        return prev1;
     }
 }

4be48aa570869a843820129d5f52bccef2353b08
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-08 02:54:49 +0530

    Time: 4 ms (63.69%), Space: 50.2 MB (71.25%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,20 +1,23 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int rowCount=grid.length;
-        int colCount=grid[0].length;
-        int[][] dp=new int[rowCount][colCount];
-        dp[rowCount-1][colCount-1]=grid[rowCount-1][colCount-1];
-        for(int i=dp[0].length-2;i>=0;i--){
-            dp[grid.length-1][i]= dp[grid.length-1][i+1]+grid[grid.length-1][i];
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] dp=new int[n][m];
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<n;i++){
+            dp[i][0]=dp[i-1][0]+grid[i][0];
         }
-        for(int i=dp.length-2;i>=0;i--){
-            dp[i][grid[0].length-1]= dp[i+1][grid[0].length-1]+grid[i][grid[0].length-1];
+
+        for(int i=1;i<m;i++){
+            dp[0][i]=dp[0][i-1]+grid[0][i];
         }
-        for(int i=dp.length-2;i>=0;i--){
-            for(int j=dp[0].length-2;j>=0;j--){
-                    dp[i][j]=Math.min(dp[i+1][j],dp[i][j+1])+grid[i][j];
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
             }
         }
-        return dp[0][0];
+
+        return dp[n-1][m-1];
     }
 }

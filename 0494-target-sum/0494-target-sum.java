8afd07f3ed15a3470196137fb0ea86d05f50e780
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-15 18:35:49 +0530

    Time: 5 ms (80.61%), Space: 45 MB (43.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,37 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int totalSum=0;
-        for(int num:nums){
-            totalSum+=num;
-        }
-        if ((totalSum + target) % 2 != 0 || Math.abs(target) > totalSum) {
-    return 0;
-}
-        int targetSum=(totalSum+target)/2;
-        int[] dp=new int[targetSum+1];
-        dp[0]=1;
-        for(int i=1;i<nums.length+1;i++){
-            for(int j=targetSum;j>=nums[i-1];j--){
-                if(j>=nums[i-1]){
-                    dp[j]+=dp[j-nums[i-1]];
+
+        int total = 0;
+        for (int num : nums)
+            total += num;
+        if (Math.abs(target) > total)
+            return 0;
+
+        if ((total + target) % 2 != 0)
+            return 0;
+
+        int sum = (total + target) / 2;
+        int n = nums.length;
+
+        int[][] dp = new int[n + 1][sum + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            int num = nums[i - 1];
+
+            for (int j = 0; j <= sum; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
                 }
             }
         }
-        return dp[targetSum];
+
+        return dp[n][sum];
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

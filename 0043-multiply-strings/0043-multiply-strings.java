974dac19e76f286d31b254167daf320f7c8b01aa
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-18 00:47:03 +0530

    Time: 3 ms (83.08%), Space: 43.6 MB (53.1%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,29 +1,52 @@
 class Solution {
-     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) return "0";
 
-        int[] result = new int[num1.length() + num2.length()];
+    public String multiply(String num1, String num2) {
 
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            int n1 = num1.charAt(i) - '0';
-            for (int j = num2.length() - 1; j >= 0; j--) {
-                int n2 = num2.charAt(j) - '0';
+        // Edge case
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
 
-                int product = n1 * n2;
-                int sum = result[i + j + 1] + product;
+        int m = num1.length();
+        int n = num2.length();
 
-                result[i + j + 1] = sum % 10;                 
-                result[i + j] += sum / 10;                     
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+
+                int product = digit1 * digit2;
+
+                int pos1 = i + j;
+                int pos2 = i + j + 1;
+
+                int sum = product + result[pos2];
+
+                result[pos2] = sum % 10;
+                result[pos1] += sum / 10;
             }
         }
 
+
         StringBuilder sb = new StringBuilder();
+
         for (int num : result) {
-            if (!(sb.length() == 0 && num == 0)) { 
-                sb.append(num);
+
+            if (sb.length() == 0 && num == 0) {
+                continue;
             }
+
+            sb.append(num);
         }
 
         return sb.toString();
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

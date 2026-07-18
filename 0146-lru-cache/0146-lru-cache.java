79df89d9cfa945682f892e678bbf4c15e050a0a3
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-18 22:31:02 +0530

    Time: 49 ms (34.4%), Space: 130.6 MB (86.04%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,82 +1,75 @@
 class LRUCache {
-
-     class Node {
-        int key, value;
-        Node prev, next;
-
-        Node(int k, int v) {
-            key = k;
-            value = v;
+    public static class Node{
+        int key;
+        int val;
+        Node prev;
+        Node next;
+        public Node(int key, int val){
+            this.key=key;
+            this.val=val;
         }
     }
-
     private int capacity;
-    private Map<Integer, Node> cache;
-    private Node head, tail;
+    private Node head;
+    private Node tail;
+    private Map<Integer,Node> map;
 
     public LRUCache(int capacity) {
-        this.capacity = capacity;
-        this.cache = new HashMap<>();
+       this.capacity=capacity;
+       map=new HashMap<>();
+       head=new Node(0,0);
+       tail=new Node(0,0);
 
-        // Dummy nodes to avoid edge cases
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
-        head.next = tail;
-        tail.prev = head;
+       head.next=tail;
+       tail.prev=head;
     }
-
+    
     public int get(int key) {
-        if (!cache.containsKey(key)) {
+        if(!map.containsKey(key)){
             return -1;
         }
 
-        Node node = cache.get(key);
-        moveToFront(node);
-        return node.value;
+        Node currentNode=map.get(key);
+        removeNode(currentNode);
+        addAtFirst(currentNode);
+        return currentNode.val;
     }
-
+    
     public void put(int key, int value) {
+        if(capacity==0){
+            return;
+        }
+        if(map.containsKey(key)){
+            Node node=map.get(key);
+            node.val=value;
+            removeNode(node);
+            addAtFirst(node);
+            return;
+        }
 
-        if (cache.containsKey(key)) {
-            Node node = cache.get(key);
-            node.value = value;
-            moveToFront(node);
-        } else {
+        Node newNode=new Node(key,value);
+        map.put(key,newNode);
+        addAtFirst(newNode);
 
-            if (cache.size() == capacity) {
-                Node lru = removeTail();
-                cache.remove(lru.key);
-            }
-
-            Node newNode = new Node(key, value);
-            cache.put(key, newNode);
-            addToFront(newNode);
+        if(capacity<map.size()){
+            Node lru=tail.prev;
+            removeNode(lru);
+            map.remove(lru.key);
         }
     }
+    private void addAtFirst(Node node) {
 
-    // ---------- Helper Methods ----------
-
-    private void addToFront(Node node) {
         node.next = head.next;
         node.prev = head;
+
         head.next.prev = node;
         head.next = node;
     }
 
     private void removeNode(Node node) {
+
         node.prev.next = node.next;
         node.next.prev = node.prev;
-    }
-
-    private void moveToFront(Node node) {
-        removeNode(node);
-        addToFront(node);
-    }
-
-    private Node removeTail() {
-        Node lru = tail.prev;
-        removeNode(lru);
-        return lru;
     }
 }
 
@@ -86,3 +79,7 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

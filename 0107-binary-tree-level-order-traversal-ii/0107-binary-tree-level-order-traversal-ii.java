fc2a7c5f749b831c8ad5ed60932a9721ebe23de3
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-23 04:37:19 +0530

    Time: 1 ms (97.22%), Space: 44.7 MB (21.06%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -41,9 +41,13 @@ class Solution {
                 }
             }
 
-            result.add(level);
+            result.add(0,level);
         }
-        Collections.reverse(result);
         return result;
     }
+    
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

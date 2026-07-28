52bcef79c2b635d9253c28b73d99d4d818ee76f9
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-28 21:13:37 +0530

    Time: 37 ms (96.8%), Space: 47.9 MB (58.88%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -64,3 +64,7 @@ class Solution {
         return 0;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

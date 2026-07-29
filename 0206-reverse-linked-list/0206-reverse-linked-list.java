50704971cfc81ae61ac2e6c0f4e4d33c6a1521d2
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-29 23:37:10 +0530

    Time: 0 ms (100%), Space: 44.2 MB (50.42%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,34 +9,16 @@
  * }
  */
 class Solution {
-    int size=1;
     public ListNode reverseList(ListNode head) {
-        int left=0;
-        int right=getsize(head)-1;
-        while(left<right){
-            ListNode leftNode=getatIndex(head,left);
-            ListNode rightNode=getatIndex(head,right);
-            int tempval=leftNode.val;
-            leftNode.val=rightNode.val;
-            rightNode.val=tempval;
-            left++;
-            right--;
+        ListNode prev=null;
+        ListNode current=head;
+        while(current!=null){
+            ListNode nextStore=current.next;
+            current.next=prev;
+            prev=current;
+            current=nextStore;
         }
-        return head;
-    }public static ListNode getatIndex(ListNode head,int ind){
-        ListNode temp=head; int i=0;
-        while(i<ind){
-            temp=temp.next;
-            i++;
-        }
-        return temp;
-    }public static int getsize(ListNode head){
-        ListNode temp=head; int size=0;
-        while(temp!=null){
-            temp=temp.next;
-            size++;
-        }
-        return size;
+        return prev;
     }
 }
 

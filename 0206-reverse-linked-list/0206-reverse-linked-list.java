19bac412ad0bcdfafd72a908db4da3362fd1f023
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-29 23:26:04 +0530

    Time: 15 ms (0.38%), Space: 43.8 MB (96.66%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,18 +9,37 @@
  * }
  */
 class Solution {
-            public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-
-        while (current != null) {
-            ListNode nextNode = current.next;  
-            current.next = prev;              
-            prev = current;                  
-            current = nextNode;          
+    int size=1;
+    public ListNode reverseList(ListNode head) {
+        int left=0;
+        int right=getsize(head)-1;
+        while(left<right){
+            ListNode leftNode=getatIndex(head,left);
+            ListNode rightNode=getatIndex(head,right);
+            int tempval=leftNode.val;
+            leftNode.val=rightNode.val;
+            rightNode.val=tempval;
+            left++;
+            right--;
         }
-
-        return prev;   
+        return head;
+    }public static ListNode getatIndex(ListNode head,int ind){
+        ListNode temp=head; int i=0;
+        while(i<ind){
+            temp=temp.next;
+            i++;
+        }
+        return temp;
+    }public static int getsize(ListNode head){
+        ListNode temp=head; int size=0;
+        while(temp!=null){
+            temp=temp.next;
+            size++;
+        }
+        return size;
     }
-    
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

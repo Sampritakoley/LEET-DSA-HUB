0e0c5c0b12bf15ff9a0ed161f1cf111246b840dd
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-30 00:04:17 +0530

    Time: 0 ms (100%), Space: 42.8 MB (83.99%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -8,36 +8,31 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
-public class Solution {
+class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || left == right) return head;
-
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-
-        ListNode prev = dummy;
-        for (int i = 1; i < left; i++) {
-            prev = prev.next;
+        ListNode dummy=new ListNode(0);
+        if (head == null || left == right)
+            return head;
+        dummy.next=head;int i=1; ListNode prev=dummy;
+        while(i<left){
+           i++;
+           prev=prev.next;
         }
-
-        ListNode reverseStart = prev.next;
-        ListNode curr = reverseStart.next;
-
-        for (int i = 0; i < right - left; i++) {
-            reverseStart.next = curr.next;
-            curr.next = prev.next;
-            prev.next = curr;
-            curr = reverseStart.next;
+        ListNode previous=null;
+        ListNode current=prev.next;
+        for (int j = 0; j < right - left + 1; j++) {
+            ListNode store=current.next;
+            current.next=previous;
+            previous=current;
+            current=store;
         }
-
+        ListNode leftNode = prev.next;
+        leftNode.next = current;
+        prev.next = previous;
         return dummy.next;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

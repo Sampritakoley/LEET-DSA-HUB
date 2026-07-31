ce16267c8b7031b5423dc7f0c262cb73a29fbce3
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-01 03:02:42 +0530

    Time: 7 ms (96.19%), Space: 47.1 MB (37.5%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.java
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -1,103 +1,116 @@
 class MyLinkedList {
-    private class Node {
+
+    class ListNode {
         int val;
-        Node next;
-        Node(int val) {
+        ListNode next;
+
+        ListNode(int val) {
             this.val = val;
         }
     }
 
-    private Node head;
+    private ListNode head;
+    private ListNode tail;
     private int size;
 
-    public MyLinkedList() {
-        head = null;
-        size = 0;
-    }
-    
-    public int get(int index) {
-        if (index < 0 || index >= size) {
-        return -1;
-        }
+    public MyLinkedList() {}
 
-        Node temp=head;
-        int i=0;
-        while(i!=index){
-            i++;
-            temp=temp.next;
-        }
-        return temp.val;
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+
+        ListNode curr = head;
+        while (index-- > 0)
+            curr = curr.next;
+
+        return curr.val;
     }
-    
+
     public void addAtHead(int val) {
-        Node first=new Node(val);
-        first.next=head;
-        head=first;
+
+        ListNode node = new ListNode(val);
+
+        node.next = head;
+        head = node;
+
+        if (size == 0)
+            tail = node;
+
         size++;
     }
-    
+
     public void addAtTail(int val) {
-        Node last=new Node(val);
-        if(head==null){
-            head=last;
-        }else{
-            Node temp=head;
-            while(temp.next!=null){
-               temp=temp.next;
-            }
-            temp.next=last;
-        }
-        size++;
-    }
-    
-    public void addAtIndex(int index, int val) {
-        if (index < 0 || index > size) {
+
+        if (size == 0) {
+            addAtHead(val);
             return;
         }
-        
+
+        ListNode node = new ListNode(val);
+
+        tail.next = node;
+        tail = node;
+
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+
+        if (index < 0 || index > size)
+            return;
+
         if (index == 0) {
             addAtHead(val);
             return;
         }
-        Node newNode=new Node(val);
-        int i=0;
-        Node temp=head;
-        while(i<index-1){
-            temp=temp.next;
-            i++;
-        }
-        newNode.next=temp.next;
-        temp.next=newNode;
-        size++;
-    }
-    
-    public void deleteAtIndex(int index) {
-        if (index < 0 || index >= size) {
+
+        if (index == size) {
+            addAtTail(val);
             return;
         }
-        
+
+        ListNode prev = head;
+
+        for (int i = 0; i < index - 1; i++)
+            prev = prev.next;
+
+        ListNode node = new ListNode(val);
+
+        node.next = prev.next;
+        prev.next = node;
+
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+
+        if (index < 0 || index >= size)
+            return;
+
         if (index == 0) {
+
             head = head.next;
-        }else{
-        int i=0;
-        Node temp=head;
-        while(i<index-1){
-            temp=temp.next;
-            i++;
+            size--;
+
+            if (size == 0)
+                tail = null;
+
+            return;
         }
-        temp.next=temp.next.next;
-        
-        }
+
+        ListNode prev = head;
+
+        for (int i = 0; i < index - 1; i++)
+            prev = prev.next;
+
+        if (prev.next == tail)
+            tail = prev;
+
+        prev.next = prev.next.next;
+
         size--;
     }
 }
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

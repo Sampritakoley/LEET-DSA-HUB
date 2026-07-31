1c79e1b8c7d61b9c47656e4c5cc542df79a7741b
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-07-31 22:25:43 +0530

    Time: 1 ms (100%), Space: 46.3 MB (77.91%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,26 +10,37 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-         ListNode dummy = new ListNode(0);
+
+        ListNode dummy = new ListNode(0);
         ListNode current = dummy;
+
         int carry = 0;
-        
+
         while (l1 != null || l2 != null || carry != 0) {
-            
-            int val1 = (l1 != null) ? l1.val : 0;
-            int val2 = (l2 != null) ? l2.val : 0;
-            
-            int sum = val1 + val2 + carry;
-            
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
             carry = sum / 10;
-            
+
             current.next = new ListNode(sum % 10);
+
             current = current.next;
-            
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
         }
-        
+
         return dummy.next;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

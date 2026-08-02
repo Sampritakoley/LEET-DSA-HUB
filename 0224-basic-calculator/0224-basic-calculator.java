aae64a3124356a335f040608eb551e8b9973b341
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-02 11:06:36 +0530

    Time: 11 ms (57.52%), Space: 46.6 MB (41.2%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,37 +1,57 @@
 class Solution {
     public int calculate(String s) {
-        int result = 0;
-        int sign = 1; 
-        int num = 0;
+
         Stack<Integer> stack = new Stack<>();
 
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+
         for (int i = 0; i < s.length(); i++) {
+
             char ch = s.charAt(i);
 
             if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
-            } else if (ch == '+') {
-                result += sign * num;
+                number = number * 10 + (ch - '0');
+            }
+
+            else if (ch == '+') {
+                result += sign * number;
+                number = 0;
                 sign = 1;
-                num = 0;
-            } else if (ch == '-') {
-                result += sign * num;
+            }
+
+            else if (ch == '-') {
+                result += sign * number;
+                number = 0;
                 sign = -1;
-                num = 0;
-            } else if (ch == '(') {
+            }
+
+            else if (ch == '(') {
                 stack.push(result);
                 stack.push(sign);
+
                 result = 0;
                 sign = 1;
-            } else if (ch == ')') {
-                result += sign * num;
-                num = 0;
-                result *= stack.pop();    
-                result += stack.pop();     
+            }
+
+            else if (ch == ')') {
+                result += sign * number;
+                number = 0;
+
+                int previousSign = stack.pop();
+                int previousResult = stack.pop();
+
+                result = previousResult + previousSign * result;
             }
         }
 
-        result += sign * num; 
+        result += sign * number;
+
         return result;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

c4e0096921b068304408b98f061716ab70686551
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-02 10:28:03 +0530

    Time: 33 ms (68.7%), Space: 100 MB (42.67%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -7,26 +7,26 @@ class MinStack {
         stack = new Stack<>();
         minStack = new Stack<>();
     }
-    
+
     public void push(int val) {
         stack.push(val);
-        
-        if (minStack.isEmpty() || val <= minStack.peek()) {
+
+        if (minStack.isEmpty()) {
             minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
         }
     }
-    
+
     public void pop() {
-        if (stack.peek().equals(minStack.peek())) {
-            minStack.pop();
-        }
         stack.pop();
+        minStack.pop();
     }
-    
+
     public int top() {
         return stack.peek();
     }
-    
+
     public int getMin() {
         return minStack.peek();
     }
@@ -35,8 +35,12 @@ class MinStack {
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
- * obj.push(val);
+ * obj.push(value);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

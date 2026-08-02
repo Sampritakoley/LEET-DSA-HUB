305f698ceecc17633ff9ac1bd3b9c81963dc4854
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-02 10:18:30 +0530

    Time: 2 ms (98.24%), Space: 42.9 MB (93.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-          Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
 
         for (char ch : s.toCharArray()) {
 
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
-            } 
+            }
             else {
                 if (stack.isEmpty()) {
                     return false;
                 }
 
-                char top = stack.peek();
+                char top = stack.pop();
 
-                if ((ch == ')' && top == '(') ||
-                    (ch == '}' && top == '{') ||
-                    (ch == ']' && top == '[')) {
-                    
-                    stack.pop();
-                } 
-                else {
+                if (ch == ')' && top != '(' ||
+                    ch == '}' && top != '{' ||
+                    ch == ']' && top != '[') {
                     return false;
                 }
             }
@@ -29,3 +25,7 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

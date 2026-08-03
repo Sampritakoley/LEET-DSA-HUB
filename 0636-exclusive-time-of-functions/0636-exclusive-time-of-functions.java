691a0a2f88f750b3f75018b279bfb52b9c6e3562
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-03 07:55:38 +0530

    Time: 12 ms (84.61%), Space: 47.4 MB (18.4%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+
+        int[] result = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        int prevTime = 0;
+
+        for (String log : logs) {
+
+            String[] parts = log.split(":");
+
+            int id = Integer.parseInt(parts[0]);
+            String type = parts[1];
+            int time = Integer.parseInt(parts[2]);
+
+            if (type.equals("start")) {
+
+                if (!stack.isEmpty()) {
+                    result[stack.peek()] += time - prevTime;
+                }
+
+                stack.push(id);
+                prevTime = time;
+
+            } else {
+
+                result[stack.peek()] += time - prevTime + 1;
+
+                stack.pop();
+
+                prevTime = time + 1;
+            }
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

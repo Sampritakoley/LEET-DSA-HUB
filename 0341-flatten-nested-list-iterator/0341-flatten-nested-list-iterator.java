232c1c76468b02220722e3d3886c1e7ec4093375
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-03 09:22:57 +0530

    Time: 4 ms (38.84%), Space: 48.1 MB (57.76%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.java
@@ -0,0 +1,46 @@
+public class NestedIterator implements Iterator<Integer> {
+
+    private Stack<NestedInteger> stack = new Stack<>();
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+
+        hasNext();
+
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        while (!stack.isEmpty()) {
+
+            NestedInteger top = stack.peek();
+
+            if (top.isInteger()) {
+                return true;
+            }
+
+            stack.pop();
+
+            List<NestedInteger> list = top.getList();
+
+            for (int i = list.size() - 1; i >= 0; i--) {
+                stack.push(list.get(i));
+            }
+        }
+
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

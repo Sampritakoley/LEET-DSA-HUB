70af0f6266f3445230a9338857908e3da8d0af94
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-05 03:27:08 +0530

    Time: 49 ms (36.58%), Space: 50.7 MB (63.51%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,27 +1,46 @@
 class Solution {
     public boolean canCross(int[] stones) {
-        int len=stones.length;
-        HashMap<Integer,HashSet<Integer>> map=new HashMap<>();
-        for(int i=0;i<stones.length;i++){
-            map.put(stones[i],new HashSet<>());
+
+        int n = stones.length;
+
+        Map<Integer, Set<Integer>> dp = new HashMap<>();
+
+        for (int stone : stones) {
+            dp.put(stone, new HashSet<>());
         }
-        map.get(0).add(1);
-        for(int i=0;i<stones.length;i++){
-            int src=stones[i];
-            for(int n:map.get(src)){
-                int desStone=src+n;
-                if(desStone==stones[len-1]){
-                    return true;
-                }
-                if(map.containsKey(desStone)){
-                    if (n-1 > 0) {
-                        map.get(desStone).add(n-1);
+
+        dp.get(0).add(0);
+
+        for (int stone : stones) {
+
+            for (int jump : dp.get(stone)) {
+
+                for (int nextJump = jump - 1;
+                     nextJump <= jump + 1;
+                     nextJump++) {
+
+                    if (nextJump <= 0) {
+                        continue;
                     }
-                    map.get(desStone).add(n);
-                    map.get(desStone).add(n+1);
+
+                    int nextPosition = stone + nextJump;
+
+                    if (dp.containsKey(nextPosition)) {
+
+                        if (nextPosition == stones[n - 1]) {
+                            return true;
+                        }
+
+                        dp.get(nextPosition).add(nextJump);
+                    }
                 }
             }
         }
+
         return false;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

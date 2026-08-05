d6fe85a104098d4ceab89342374587f893217fc1
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-05 12:58:20 +0530

    Time: 3 ms (92.19%), Space: 46.9 MB (68%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
@@ -1,28 +1,53 @@
-import java.util.*;
-
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
         PriorityQueue<Integer> maxHeap =
-                new PriorityQueue<>(Collections.reverseOrder());
+            new PriorityQueue<>((a, b) -> b - a);
 
         int fuel = startFuel;
-        int i = 0;
         int stops = 0;
+        int previousPosition = 0;
 
-        while (fuel < target) {
+        for (int[] station : stations) {
 
-            while (i < stations.length && stations[i][0] <= fuel) {
-                maxHeap.offer(stations[i][1]);
-                i++;
+            int position = station[0];
+            int stationFuel = station[1];
+
+            int distance = position - previousPosition;
+
+            fuel -= distance;
+            while (fuel < 0 && !maxHeap.isEmpty()) {
+
+                fuel += maxHeap.poll();
+                stops++;
             }
 
-            if (maxHeap.isEmpty())
+            if (fuel < 0) {
                 return -1;
+            }
+
+            maxHeap.offer(stationFuel);
+
+            previousPosition = position;
+        }
+
+        int distanceToTarget = target - previousPosition;
+
+        fuel -= distanceToTarget;
+
+        while (fuel < 0 && !maxHeap.isEmpty()) {
 
             fuel += maxHeap.poll();
             stops++;
         }
 
+        if (fuel < 0) {
+            return -1;
+        }
+
         return stops;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

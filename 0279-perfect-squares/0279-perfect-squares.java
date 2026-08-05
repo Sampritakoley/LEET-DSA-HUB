94254a12d3d183d8903eb542f5db658d9c289125
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-06 02:50:51 +0530

    Time: 37 ms (42.93%), Space: 44 MB (94.49%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,16 +1,32 @@
+
 class Solution {
     public int numSquares(int n) {
+
         int[] dp = new int[n + 1];
 
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        Arrays.fill(dp, n);
+
         dp[0] = 0;
 
         for (int i = 1; i <= n; i++) {
+
             for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+
+                int square = j * j;
+
+                dp[i] = Math.min(
+                    dp[i],
+                    dp[i - square] + 1
+                );
             }
         }
 
         return dp[n];
     }
 }
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

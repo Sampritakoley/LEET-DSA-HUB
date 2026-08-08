7aa6fdbb89ed7b2e0e0c1ac78468152baed233b0
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-09 03:07:14 +0530

    Time: 1 ms (91.81%), Space: 45.1 MB (38.28%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -19,7 +19,7 @@ class Solution {
             for (int i = 0; i <= n; i++) {
                parent[i] = i;
                rank[i] = 0;
-            }
+            } 
         }
         public int parent(int u){
             if(parent[u]!=u){
@@ -46,3 +46,7 @@ class Solution {
         }
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

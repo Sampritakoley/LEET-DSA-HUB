196a3d7fb34a933978b6ba0ff4248cdc2d79f2ee
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-09 11:52:32 +0530

    Time: 0 ms (100%), Space: 48.1 MB (72.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,70 @@
 class Solution {
+
     public int[] searchRange(int[] nums, int target) {
-        int first = findLowerBound(nums, target);
-        int last = findUpperBound(nums, target);
+
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
         return new int[]{first, last};
     }
 
-    public static int findLowerBound(int[] nums, int target){
-        int low=0;
-        int high=nums.length-1;
-        int ans=-1;
-        while(low<=high){
-           int mid=low+(high-low)/2;
-           if(nums[mid]==target){
-                ans=mid;
-                high=mid-1;
-           }else if(nums[mid]<target){
-                low=mid+1;
-           }else{
-                 high=mid-1;
-           }
+    private int findFirst(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int answer = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < target) {
+
+                low = mid + 1;
+
+            } else if (nums[mid] > target) {
+
+                high = mid - 1;
+
+            } else {
+                answer = mid;
+
+                high = mid - 1;
+            }
         }
-        return ans;
+
+        return answer;
     }
 
-    public static int findUpperBound(int[] nums, int target){
-        int low=0;
-        int high=nums.length-1;
-        int ans=-1;
-        while(low<=high){
-           int mid=low+(high-low)/2;
-           if(nums[mid]==target){
-                ans=mid;
-                low=mid+1;
-           }else if(nums[mid]<target){
-                low=mid+1;
-           }else{
-               high=mid-1;
-           }
+    private int findLast(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int answer = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < target) {
+
+                low = mid + 1;
+
+            } else if (nums[mid] > target) {
+
+                high = mid - 1;
+
+            } else {
+                answer = mid;
+                low = mid + 1;
+            }
         }
-        return ans;
+
+        return answer;
     }
 }
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

ccbfda150226cbae0829368940c10b034eb65854
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-09 11:47:51 +0530

    Time: 1 ms (97.81%), Space: 42.7 MB (43.5%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,28 +1,32 @@
+
 class Solution {
     public int mySqrt(int x) {
+
         if (x < 2) {
             return x;
         }
 
-        int left = 1;
-        int right = x / 2;
-        int ans = 0;
+        int low = 1;
+        int high = x;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while (low <= high) {
 
-            if ((long) mid * mid == x) {
-                return mid;
-            }
-            else if ((long) mid * mid < x) {
-                ans = mid;
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
+            int mid = low + (high - low) / 2;
+
+            long square = (long) mid * mid;
+
+            if (square <= x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
-        return ans;
+        return high;
     }
 }
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

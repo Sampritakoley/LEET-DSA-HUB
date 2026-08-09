23fa44eab9d41a4c26d784d42ea9cd6002c786cb
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-10 02:05:31 +0530

    Time: 0 ms (100%), Space: 45 MB (66.61%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,33 +1,45 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-       int left=0;
-       int right=nums.length-1;
 
-       while(left<=right){
-        int mid=left+(right-left)/2;
-        if(nums[mid]==target){
-            return true;
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+
+            }
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
         }
 
-           if(nums[mid]==nums[left] && nums[right]==nums[left]){
-               left++;
-               right--;
-               continue;
-           }
-           if(nums[left]<=nums[mid]){
-                if(target>=nums[left] && nums[mid]>target){
-                    right=mid-1;
-                }else{
-                    left=mid+1;
-                }
-           }else{
-              if(target>nums[mid] && target<=nums[right]){
-                  left=mid+1;
-              }else{
-                  right=mid-1;
-              }
-           }
-       }
-       return false;
+        return false;
     }
 }
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

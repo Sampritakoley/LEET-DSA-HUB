01c295b3e247f9cba0c11b3ed67ca4f14620f155
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-10 08:33:51 +0530

    Time: 45 ms (91.79%), Space: 91 MB (63.08%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -0,0 +1,46 @@
+
+class Solution {
+
+    public long minimumTime(int[] time, int totalTrips) {
+
+        long low = 1;
+
+        long minTime = Long.MAX_VALUE;
+
+        for (int t : time) {
+            minTime = Math.min(minTime, t);
+        }
+        long high = minTime * totalTrips;
+
+        while (low < high) {
+
+            long mid = low + (high - low) / 2;
+
+            long trips = 0;
+
+            for (int t : time) {
+
+                trips += mid / t;
+                if (trips >= totalTrips) {
+                    break;
+                }
+            }
+
+            if (trips >= totalTrips) {
+                high = mid;
+
+            } else {
+
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

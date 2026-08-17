9dd9eb44c20c8402cbc557b47a0f7f54f755e8d7
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-18 05:01:16 +0530

    Time: 8 ms (49.64%), Space: 46.4 MB (11.59%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,20 +1,31 @@
+import java.util.*;
+
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        
+        int n = s.length();
+        
         Set<String> set = new HashSet<>(wordDict);
-        boolean[] dp=new boolean[s.length()+1];
-        int n=s.length()+1;
-        dp[0]=true;
-        for(int i=1;i<n;i++){
-            for(int j=i-1;j>=0;j--){
-                String str=s.substring(j,i);
-                if(set.contains(str)){
-                    if(dp[j]){
-                        dp[i]=true;
-                        break;
-                    }
+        
+        boolean[] dp = new boolean[n + 1];
+        
+        dp[0] = true;
+        
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 0; j < i; j++) {
+                
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        return dp[s.length()];
+        
+        return dp[n];
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

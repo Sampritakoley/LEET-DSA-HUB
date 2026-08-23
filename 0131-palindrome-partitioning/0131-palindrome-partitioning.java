020531c5b16991a78131ac42ba503fda9fef164f
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-23 09:40:56 +0530

    Time: 7 ms (96.98%), Space: 65.4 MB (40.79%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,37 +1,50 @@
 class Solution {
+
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        ArrayList<String> list = new ArrayList<>();
-        find(res, list, s);  
-        return res;   
+
+        List<List<String>> result = new ArrayList<>();
+
+        backtrack(0, s, new ArrayList<>(), result);
+
+        return result;
     }
 
-    public static void find(List<List<String>> res, ArrayList<String> list, String Qus) {
-        if (Qus.isEmpty()) {
-            res.add(new ArrayList<>(list));
+    private void backtrack(
+            int index,
+            String s,
+            List<String> current,
+            List<List<String>> result) {
+
+        if (index == s.length()) {
+            result.add(new ArrayList<>(current));
             return;
         }
-        for (int i = 1; i <= Qus.length(); i++) {
-            String newQ = Qus.substring(i);
-            String newA = Qus.substring(0, i);
-            if (isPalindrome(newA)) {
-                list.add(newA);
-                find(res, list, newQ);
-                list.remove(list.size() - 1);
+        for (int end = index; end < s.length(); end++) {
+
+            if (isPalindrome(s, index, end)) {
+                current.add(s.substring(index, end + 1));
+                backtrack(end + 1, s, current, result);
+                current.remove(current.size() - 1);
             }
         }
     }
 
-    public static boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
+    private boolean isPalindrome(String s, int left, int right) {
+
         while (left < right) {
+
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
+
             left++;
             right--;
         }
+
         return true;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

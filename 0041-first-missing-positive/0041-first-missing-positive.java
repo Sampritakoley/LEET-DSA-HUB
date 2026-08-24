a34ea6293506bd3e7fd100ca937a7a5509f9c480
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-24 16:16:08 +0530

    Time: 1 ms (99.96%), Space: 71.2 MB (73.04%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,21 +1,38 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-          int n = nums.length;
 
+        int n = nums.length;
+
+        // Put each number in its correct position
         for (int i = 0; i < n; i++) {
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                int temp = nums[nums[i] - 1];
-                nums[nums[i] - 1] = nums[i];
-                nums[i] = temp;
+
+            while (
+                nums[i] >= 1 &&
+                nums[i] <= n &&
+                nums[nums[i] - 1] != nums[i]
+            ) {
+
+                int correctIndex = nums[i] - 1;
+
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
             }
         }
 
+        // Find first number that is not in its correct position
         for (int i = 0; i < n; i++) {
+
             if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
 
+        // All 1...n are present
         return n + 1;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

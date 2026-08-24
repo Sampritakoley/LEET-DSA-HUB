5f757ec5f785fe9eab5fec8c81a9f9848709bd27
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-24 09:51:16 +0530

    Time: 5 ms (73.14%), Space: 43.4 MB (59.77%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int hIndex(int[] citations) {
+
+        Arrays.sort(citations);
+
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int h = n - i;
+
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

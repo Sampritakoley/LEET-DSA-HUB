b8c2eb5f80c3fb8910d3c510a196014785562c0c
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-24 13:11:09 +0530

    Time: 23 ms (92.43%), Space: 139.5 MB (77.28%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int prefixSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            prefixSum += nums[i];
+
+            int remainder = prefixSum % k;
+
+            if (map.containsKey(remainder)) {
+
+                int previousIndex = map.get(remainder);
+                if (i - previousIndex >= 2) {
+                    return true;
+                }
+
+            } else {
+                map.put(remainder, i);
+            }
+        }
+
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

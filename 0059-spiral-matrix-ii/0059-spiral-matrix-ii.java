87f999c1bd122bb2a12a9250298e1fcf46a87c0c
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-25 22:08:33 +0530

    Time: 0 ms (100%), Space: 42.5 MB (97.74%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,38 +1,42 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] res=new int[n][n];
-        int count=1;
-        int top=0;int bottom=n-1;int left=0;int right=n-1;
-        while(true){
-            for(int i=left;i<=right;i++){
-                res[top][i]=count++;
+
+        int[][] matrix = new int[n][n];
+
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = n - 1;
+
+        int num = 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int col = left; col <= right; col++) {
+                matrix[top][col] = num++;
             }
             top++;
-            if(top>bottom){
-                return res;
-            }
-            for(int i=top;i<=bottom;i++){
-                res[i][right]=count++;
+
+            for (int row = top; row <= bottom; row++) {
+                matrix[row][right] = num++;
             }
             right--;
-            if(left>right){
-               return res;
-            }
-            for(int i=right;i>=left;i--){
-                res[bottom][i]=count++;
+
+            for (int col = right; col >= left; col--) {
+                matrix[bottom][col] = num++;
             }
             bottom--;
-            if(top>bottom){
-                return res;
-            }
 
-            for(int i=bottom;i>=top;i--){
-                res[i][left]=count++;
+            for (int row = bottom; row >= top; row--) {
+                matrix[row][left] = num++;
             }
             left++;
-            if(left>right){
-               return res;
-            }
         }
+
+        return matrix;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

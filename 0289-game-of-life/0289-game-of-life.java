37116c8635d74266a71797235a97e472cbff32b6
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-28 02:32:47 +0530

    Time: 0 ms (100%), Space: 42.9 MB (95.79%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,58 +1,59 @@
 class Solution {
+
     public void gameOfLife(int[][] board) {
-         for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                CounterResult res=findAdjacentVal(board,i,j);
-                if(board[i][j]==0){
-                   if(res.oneCount==3){
-                    board[i][j]=-1;
-                   }
-                }else{
-                    if(res.oneCount<2){
-                        board[i][j]=-2;
-                    }else if(res.oneCount==2||res.oneCount==3){
-                        board[i][j]=1;
-                    }else if(res.oneCount>3){
-                        board[i][j]=-2;
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                int liveNeighbors = 0;
+                for (int k = 0; k < 8; k++) {
+
+                    int nr = row + dr[k];
+                    int nc = col + dc[k];
+                    if (nr >= 0 && nr < rows &&
+                        nc >= 0 && nc < cols) {
+                        if (board[nr][nc] == 1 ||
+                            board[nr][nc] == 3) {
+
+                            liveNeighbors++;
+                        }
+                    }
+                }
+
+                if (board[row][col] == 1) {
+
+                    if (liveNeighbors < 2 || liveNeighbors > 3) {
+                        board[row][col] = 3;
+                    }
+
+                } else if (board[row][col] == 0) {
+
+                    if (liveNeighbors == 3) {
+                        board[row][col] = 2;
                     }
                 }
             }
         }
 
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==-1){
-                    board[i][j]=1;
-                }else if(board[i][j]==-2){
-                     board[i][j]=0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                if (board[row][col] == 2) {
+                    board[row][col] = 1;
+                } else if (board[row][col] == 3) {
+                    board[row][col] = 0;
                 }
             }
         }
     }
-     
-     public static class CounterResult {
-        public int zeroCount;
-        public int oneCount;
-    }
-    //dead to alive->-1   live to dead->-2
-    public static CounterResult findAdjacentVal(int[][] board,int i,int j){
-         CounterResult res=new CounterResult();
-         int[] x=new int[]{-1,-1,0,1,1,1,0,-1};
-         int[] y=new int[]{0,1,1,1,0,-1,-1,-1};
-    
-         for(int k=0;k<8;k++){
-            if(i+x[k]<0 || j+y[k]<0 ||i+x[k]>=board.length ||j+y[k]>=board[0].length){
-                continue;
-            }
-
-
-            int val=board[i+x[k]][j+y[k]];
-            if(val==0 || val==-1){
-                res.zeroCount++;
-            }else{
-                res.oneCount++;
-            }
-         }
-         return res;
-    }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

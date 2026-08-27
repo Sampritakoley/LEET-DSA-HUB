d803d538f825073eb163448f19911be5c668edc9
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-28 02:20:22 +0530

    Time: 1 ms (100%), Space: 46.4 MB (82.87%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,27 +1,51 @@
 class Solution {
+
     public int maxAreaOfIsland(int[][] grid) {
-        boolean[][] visited=new boolean[grid.length][grid[0].length];
-        int maxArea=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(!visited[i][j] && grid[i][j]==1){
-                    int area=dfs(grid,visited,i,j);
-                    maxArea=maxArea<area?area:maxArea;
-                } 
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int maxArea = 0;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                if (grid[row][col] == 1) {
+
+                    int currentArea = dfs(grid, row, col);
+
+                    maxArea = Math.max(maxArea, currentArea);
+                }
             }
         }
+
         return maxArea;
     }
-    public int dfs(int[][] grid,boolean[][] visited,int i, int j){
-        if(i<0 || j< 0 || i>=grid.length || j>= grid[0].length|| visited[i][j] || grid[i][j]!=1){
+
+    private int dfs(int[][] grid, int row, int col) {
+
+        if (row < 0 || row >= grid.length ||
+            col < 0 || col >= grid[0].length) {
             return 0;
         }
-         visited[i][j]=true;
-         return 1+ 
-         dfs(grid,visited,i-1,j)+
-         dfs(grid,visited,i+1,j)+
-         dfs(grid,visited,i,j+1)+
-         dfs(grid,visited,i,j-1);
 
+        if (grid[row][col] == 0) {
+            return 0;
+        }
+
+        grid[row][col] = 0;
+
+        int area = 1;
+
+        area += dfs(grid, row - 1, col); 
+        area += dfs(grid, row + 1, col); 
+        area += dfs(grid, row, col - 1); 
+        area += dfs(grid, row, col + 1);
+
+        return area;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

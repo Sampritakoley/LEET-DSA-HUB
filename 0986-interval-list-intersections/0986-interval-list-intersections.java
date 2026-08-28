aedab9c4381ddfb508828b55a4cb6871e885622b
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-08-28 13:38:13 +0530

    Time: 4 ms (50.46%), Space: 47.7 MB (6.06%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    public int[][] intervalIntersection(
+            int[][] firstList,
+            int[][] secondList) {
+
+        List<int[]> result = new ArrayList<>();
+
+        int i = 0;
+        int j = 0;
+
+        while (i < firstList.length &&
+               j < secondList.length) {
+
+            int start = Math.max(
+                    firstList[i][0],
+                    secondList[j][0]
+            );
+
+            int end = Math.min(
+                    firstList[i][1],
+                    secondList[j][1]
+            );
+
+            if (start <= end) {
+                result.add(new int[]{start, end});
+            }
+
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

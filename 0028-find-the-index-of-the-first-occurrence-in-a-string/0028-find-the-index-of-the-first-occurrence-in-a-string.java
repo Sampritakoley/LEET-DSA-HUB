64665aee8c0863cbcdad153ea475335670222379
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-09-03 16:58:54 +0530

    Time: 2 ms (13.67%), Space: 43 MB (67.89%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,72 @@
 class Solution {
+
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+
+        if (needle.length() == 0) {
+            return 0;
+        }
+
+        int[] lps = buildLPS(needle);
+
+        int i = 0; 
+        int j = 0; 
+
+        while (i < haystack.length()) {
+
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == needle.length()) {
+                    return i - j;
+                }
+
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private int[] buildLPS(String pattern) {
+
+        int n = pattern.length();
+        int[] lps = new int[n];
+
+        int len = 0;
+        int i = 1;
+
+        while (i < n) {
+
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+
+                len++;
+                lps[i] = len;
+                i++;
+
+            } else {
+
+                if (len != 0) {
+                    len = lps[len - 1];
+
+                } else {
+
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

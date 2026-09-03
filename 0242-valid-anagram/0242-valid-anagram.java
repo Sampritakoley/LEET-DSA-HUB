9a22ba89928144f001cb9f63b4893bac913f49ae
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-09-03 17:38:45 +0530

    Time: 2 ms (99.65%), Space: 44.5 MB (59.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,32 @@
 class Solution {
+
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
+
+        if (s.length() != t.length()) {
             return false;
         }
-        int[] arr=new int[26];
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
+
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
 
-        for(int val:arr){
-            if(val!=0){
+        for (char ch : t.toCharArray()) {
+            freq[ch - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (count != 0) {
                 return false;
             }
         }
-        return true;
 
+        return true;
     }
 }
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

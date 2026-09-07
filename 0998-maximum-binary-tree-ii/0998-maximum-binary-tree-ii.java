44570d7f388bb7c19cfb9c9d32a073376f6597de
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-09-07 16:15:59 +0530

    Time: 0 ms (100%), Space: 43.6 MB (14.39%) - LeetHub

---
+++ 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+
+        if (root == null || val > root.val) {
+
+            TreeNode node = new TreeNode(val);
+
+            node.left = root;
+
+            return node;
+        }
+
+        root.right = insertIntoMaxTree(root.right, val);
+
+        return root;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

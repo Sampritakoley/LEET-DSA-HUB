28bd0509583a534ec0b9256c8a1ecd1230894ece
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-09-09 01:00:21 +0530

    Time: 2 ms (51.61%), Space: 46.5 MB (54.37%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,52 +1,42 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
-        Queue<Node> q=new LinkedList<>();
-        if(root==null){
-            return root;
+        if (root == null) {
+            return null;
         }
-        q.offer(root);
-        while(q.size()>0){
-            int levelsize=q.size();
-            Node prev=null;
-            for(int i=0;i<levelsize;i++){
-                Node current=q.poll();
-                if(current.left!=null){
-                    q.offer(current.left);
-                }
-                if(current.right!=null){
-                    q.offer(current.right);
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            Node previous = null;
+
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
+
+                if (previous != null) {
+                    previous.next = current;
                 }
 
-                if(prev!=null){
-                    prev.next=current;
+                previous = current;
+
+                if (current.left != null) {
+                    queue.offer(current.left);
                 }
-                prev=current;
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
             }
-            prev.next=null;
+
+            previous.next = null;
         }
+
         return root;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

7425e55e38ff44edd646a9b9fa4f14c73f44807b
Samprita Koley <109060246+Sampritakoley@users.noreply.github.com>
2026-09-09 11:19:43 +0530

    Time: 0 ms (100%), Space: 46.1 MB (93.36%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -1,17 +1,22 @@
 class Solution {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
-        return RemoveNode(root,target);
-    }public TreeNode RemoveNode(TreeNode root,int target){
-        if(root==null){
-            return null;
-        }
-        
-        root.left=RemoveNode(root.left,target);
-        root.right=RemoveNode(root.right,target);
 
-        if(root.left==null && root.right==null && root.val==target){
+        if (root == null) {
             return null;
         }
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if (root.val == target
+                && root.left == null
+                && root.right == null) {
+            return null;
+        }
+
         return root;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
